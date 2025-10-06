0ae85247646c11eedaa340b754b7e736120dde5d
tranlong6226-dot <tranlong6226@gmail.com>
2025-10-06 07:45:54 +0700

    feat: upload code to branch long

---
+++ Mylib/Motor.c
@@ -0,0 +1,174 @@
+
+
+
+#include "Motor.h"
+#include <math.h>
+
+
+#ifndef MOTOR_RPS_MAX
+#define MOTOR_RPS_MAX    (330.0f/60.0f) /* ~5.5 rps n?u motor ~330rpm */
+#endif
+
+#ifndef ENCODER_PPR
+#define ENCODER_PPR      1320.0f   // v? d?: PPR (ticks/rev) do du?c c?a encoder
+#endif
+
+#ifndef WHEEL_RADIUS_M
+#define WHEEL_RADIUS_M   0.034f    // 34 mm
+#endif
+
+#ifndef TRACK_WIDTH_M
+#define TRACK_WIDTH_M    0.30f     // 30 cm
+#endif
+
+
+
+
+
+
+
+
+
+
+	double encoder_progress = 0;
+	double encoder_output = 0;
+	double encoder_target = 0;
+	uint16_t count = 0;
+	uint32_t prevtime = 0;
+
+
+	// Init Motor
+	void Motor_Init(Motor *_motor,
+						Motor_id id,
+						GPIO_TypeDef *_IN1_Port, uint16_t _IN1_Pin,
+						GPIO_TypeDef *_IN2_Port, uint16_t _IN2_Pin,
+						TIM_HandleTypeDef *htim_pwm, uint32_t Channel,
+						TIM_HandleTypeDef *htim_encoder,
+						float kp, float ki, float kd)
+		{
+			// G?n ID
+			_motor->id = id;
+
+			// Luu th?ng tin ch?n di?u khi?n chi?u
+			_motor->IN1_Port = _IN1_Port;
+			_motor->IN1_Pin  = _IN1_Pin;
+			_motor->IN2_Port = _IN2_Port;
+			_motor->IN2_Pin  = _IN2_Pin;
+
+			// PWM
+			_motor->htim_pwm = htim_pwm;
+			_motor->Channel  = Channel;
+
+
+			// Encoder
+			_motor->htim_encoder = htim_encoder;
+			_motor->encoder_count = 0;
+			_motor->prev_count    = 0;
+			_motor->delta_count   = 0;
+			_motor->progress_count = 0;
+
+			// PID
+			_motor->kp = kp;
+			_motor->ki = ki;
+			_motor->kd = kd;
+
+			// Speed
+			_motor->cur_speed    = 0.0;
+			_motor->target_speed = 0.0;
+			_motor->Pid_output = 0.0;
+
+			// Start PWM
+			HAL_TIM_PWM_Start(_motor->htim_pwm, _motor->Channel);
+
+			// Kh?i d?ng Encoder
+			HAL_TIM_Encoder_Start(_motor->htim_encoder, TIM_CHANNEL_ALL);
+		}
+		
+void Motor_UpdateSpeed(Motor *_motor, float new);
+void Motor_GetSpeed(Motor *_motor)
+	{
+		_motor->encoder_count = __HAL_TIM_GET_COUNTER(_motor->htim_encoder);
+		_motor->delta_count = _motor->encoder_count - _motor->prev_count;
+
+		_motor->cur_speed = ( _motor->delta_count/ ENCODER_PPR) * (60.0 / (2 / 1000.0)); 
+		_motor->prev_count = _motor->encoder_count;
+
+	}
+	
+	
+void Motor_SetPwm(Motor *motor)
+	{
+		if(motor->Pid_output > 3199)
+		{
+			motor->Pid_output = 3199;
+		}
+		else if(motor->Pid_output < -3199)
+		{
+			motor->Pid_output = -3199;
+		}
+	if(motor->id == RIGHT)
+		{
+			if(motor->Pid_output > 0)
+			{
+				HAL_GPIO_WritePin(motor->IN1_Port, motor->IN1_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(motor->IN2_Port, motor->IN2_Pin, GPIO_PIN_RESET);
+			}
+			else if(motor->Pid_output < 0)
+			{
+				HAL_GPIO_WritePin(motor->IN1_Port, motor->IN1_Pin,GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(motor->IN2_Port, motor->IN2_Pin, GPIO_PIN_SET);
+			}
+			else
+			{
+				HAL_GPIO_WritePin(motor->IN1_Port, motor->IN1_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(motor->IN2_Port, motor->IN2_Pin, GPIO_PIN_RESET);
+			}
+			motor->htim_pwm->Instance->CCR2 =(uint32_t)((fabs)(motor->Pid_output) );
+			}
+		else if (motor->id == LEFT)
+		{
+			if(motor->Pid_output > 0)
+			{
+				HAL_GPIO_WritePin(motor->IN1_Port, motor->IN1_Pin,GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(motor->IN2_Port, motor->IN2_Pin,GPIO_PIN_SET);
+			}
+			else if(motor->Pid_output < 0)
+			{
+				HAL_GPIO_WritePin(motor->IN1_Port, motor->IN1_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(motor->IN2_Port, motor->IN2_Pin, GPIO_PIN_RESET);
+			}
+			else
+			{
+				HAL_GPIO_WritePin(motor->IN1_Port, motor->IN1_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(motor->IN2_Port, motor->IN2_Pin, GPIO_PIN_RESET);
+			}
+			motor->htim_pwm->Instance->CCR1 =(uint32_t)((fabs)(motor->Pid_output) );
+
+		}
+
+
+	}
+void Motor_SetTarget(Motor*motor, double target)
+	{
+		motor->target_speed = target;
+	}
+
+void Drive_VW(Motor *mL, Motor *mR, float v_mps, float w_radps)
+{
+    const float half_track = TRACK_WIDTH_M * 0.5f;
+    const float two_pi_R   = 2.0f * 3.1415926f * WHEEL_RADIUS_M;
+
+    float vL = v_mps - w_radps * half_track;   // [m/s]
+    float vR = v_mps + w_radps * half_track;   // [m/s]
+
+    double rpsL = (double)(vL / two_pi_R);
+    double rpsR = (double)(vR / two_pi_R);
+
+    if (rpsL >  MOTOR_RPS_MAX) rpsL =  MOTOR_RPS_MAX;
+    if (rpsL < -MOTOR_RPS_MAX) rpsL = -MOTOR_RPS_MAX;
+    if (rpsR >  MOTOR_RPS_MAX) rpsR =  MOTOR_RPS_MAX;
+    if (rpsR < -MOTOR_RPS_MAX) rpsR = -MOTOR_RPS_MAX;
+
+    Motor_SetTarget(mL, rpsL);
+    Motor_SetTarget(mR, rpsR);
+}
